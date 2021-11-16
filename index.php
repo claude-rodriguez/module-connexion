c656c184602cef953c4c56e5544832068ca19bf3
rodriguez <claude.rodriguez@laplateforme.io>
2021-11-16 11:54:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="connexion.css">
+    <title>ACCUEIL</title>
+</head>
+
+    <body>
+
+        <header>
+    
+        
+        </header>
+
+        <main>
+           
+                
+            
+
+        </main>
+        
+        <footer>
+            
+        </footer>
+            
+    </body>
+</html>
